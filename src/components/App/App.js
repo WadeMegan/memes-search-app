a0fb4b5307a66aef2c5e0275fce8972f630502e0
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-06-29 16:55:37 -0500

    added footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import SearchPage from '../SearchPage/SearchPage'
 import MemePage from '../MemePage/MemePage.js'
 
+
 class App extends Component {
 
   render(){
@@ -19,7 +20,12 @@ class App extends Component {
         </Switch>
       </main>
       <footer>
-        
+        <p>Created by <a href="https://portfolio.wademegan.now.sh/">Megan Wade</a></p>
+        <ul>
+          <li><a href="https://www.linkedin.com/in/megan-wade-909129124/" target="_blank" rel="noopener noreferrer" aria-label="linkedin link"><i className="fab fa-linkedin fa-2x"></i></a></li>
+          <li><a href="mailto:meganwade96@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="email link"><i className="fas fa-envelope-square fa-2x"></i></a></li>
+          <li><a href="https://github.com/WadeMegan" target="_blank" rel="noopener noreferrer" aria-label="github link"><i className="fab fa-github-square fa-2x"></i></a></li>
+        </ul>
       </footer>
       </>
     )
